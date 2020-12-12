ab33d619b421189c881de124c6a2c57bcd88b065
ayushi gupta <ayushi3101gupta@gmail.com>
2020-12-12 15:58:35 +0100

    Change index page to grid of cards

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
   def show_terrace_photo(terrace)
     photo = terrace.photos.sample
     if photo
-      cl_image_tag photo.key, height: 300, class: "card-img-top"
+      cl_image_tag photo.key, height: 150, width: 200, class: "card-img-top"
     else
       image_tag('placeholder.jpg')
     end
